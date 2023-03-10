77c7a92f72f9b950abc61ac465d72ed99251de4f
GURUCHARAN TANNEERU <65709955+gurucharan15@users.noreply.github.com>
2023-03-10 09:38:22 +0530

    3

---
+++ components/aboutPage/About.tsx
@@ -0,0 +1,26 @@
+import AboutMe from "./AboutMe"
+import BrandLogos from "./BrandLogos"
+import Facts from "./Facts"
+import MyServices from "./MyServices"
+import Prices from "./Prices"
+import Testimonials from "./Testimonials"
+import Title from "../Title"
+
+export default function About() {
+  return (
+    <section className="h-full overflow-y-scroll myScroll">
+      <Title name="about me" />
+      <AboutMe />
+
+      <Title name="my services" />
+      <MyServices />
+
+     
+      <Title name="testimonials" />
+      <Testimonials />
+
+  
+    </section>
+
+  )
+}
