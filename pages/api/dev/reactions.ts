c3da114238f9594e61642e55a33baa720cafedb6
GURUCHARAN TANNEERU <65709955+gurucharan15@users.noreply.github.com>
2023-03-12 23:20:57 +0530

    Add files via upload

--- pages/api/dev/reactions.ts
+++ pages/api/dev/reactions.ts
@@ -6,7 +6,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const response = await fetch(
-    "https://dev.to/api/articles?username=gurucharan15&per_page=9000000000000000000"
+    "https://dev.to/api/articles?username=gurucharan15&per_page=7cs6CF4mWAKmsdG2irKpcyso"
   )
   const articles = await response.json()
 
