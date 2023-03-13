653126a59bb86d25578ae5911160a7d46b201469
GURUCHARAN TANNEERU <65709955+gurucharan15@users.noreply.github.com>
2023-03-13 11:06:20 +0530

    Add files via upload

--- components/contactPage/Location.tsx
+++ components/contactPage/Location.tsx
@@ -4,7 +4,7 @@ export default function Location() {
   return (
     <div className="p-12">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30696112.08126717!2d64.43848300685325!3d20.088775406435456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1678685496653!5m2!1sen!2sin"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d310723.7661318751!2d80.62595942896866!3d16.488692277576444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a35eff9482d944b%3A0x939b7e84ab4a0265!2sVijayawada%2C%20Andhra%20Pradesh!5e0!3m2!1sen!2sin!4v1678685737421!5m2!1sen!2sin"
         style={{ border: 0 } as React.CSSProperties}
         allowFullScreen={false}
         loading="lazy"
