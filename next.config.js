dd327f682e10138546e09c6063028ed4ff718284
GURUCHARAN TANNEERU <65709955+gurucharan15@users.noreply.github.com>
2025-08-17 14:06:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["media.graphassets.com", "res.cloudinary.com"],
+    domains: ["ap-south-1.graphassets.com", "res.cloudinary.com"],
   },
 }
 
